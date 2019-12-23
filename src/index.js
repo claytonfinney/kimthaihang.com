7082e363a563480ae95135aad371484e75a7df5e
claytonfinney <clay@2600hz.com>
2019-12-23 01:26:16 -0800

    added font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+
+import './fonts/silka-regular-webfont.eot';
+import './fonts/silka-regular-webfont.woff2';
+import './fonts/silka-regular-webfont.woff';
+import './fonts/silka-regular-webfont.ttf';
+
+import './fonts/silka-regularitalic-webfont.eot';
+import './fonts/silka-regularitalic-webfont.woff2';
+import './fonts/silka-regularitalic-webfont.woff';
+import './fonts/silka-regularitalic-webfont.ttf';
+
 import FullPage from './components/fullPage';
 
 ReactDOM.render(<FullPage />, document.getElementById('root'));
