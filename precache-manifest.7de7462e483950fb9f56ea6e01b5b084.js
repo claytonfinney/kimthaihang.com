22673a304b28955a0e1249b220b0add8fbbab151
claytonfinney <clay@2600hz.com>
2020-03-30 13:56:03 -0700

    Updates

---
+++ precache-manifest.7de7462e483950fb9f56ea6e01b5b084.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab19a73320ca9707a63b233797f560d",
+    "url": "kimthaihang.com/index.html"
+  },
+  {
+    "revision": "731e9d9a6502cdebf052",
+    "url": "kimthaihang.com/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "def3e07e8322a3503ff8",
+    "url": "kimthaihang.com/static/css/main.1c791ee8.chunk.css"
+  },
+  {
+    "revision": "731e9d9a6502cdebf052",
+    "url": "kimthaihang.com/static/js/2.f8b125c0.chunk.js"
+  },
+  {
+    "revision": "def3e07e8322a3503ff8",
+    "url": "kimthaihang.com/static/js/main.770eca48.chunk.js"
+  },
+  {
+    "revision": "653da5e00b1bf01478c7",
+    "url": "kimthaihang.com/static/js/runtime~main.c659374e.js"
+  },
+  {
+    "revision": "0b97f3edea70190ed210ea0ce942206a",
+    "url": "kimthaihang.com/static/media/california.0b97f3ed.jpg"
+  },
+  {
+    "revision": "b5fab27cb22fc56d6a487a4504de52c7",
+    "url": "kimthaihang.com/static/media/cvs3.b5fab27c.jpg"
+  },
+  {
+    "revision": "ea874c58590e820384b61dd32a05803a",
+    "url": "kimthaihang.com/static/media/florence.ea874c58.jpg"
+  },
+  {
+    "revision": "96c8a99eaad24382b725c30bc93f21b0",
+    "url": "kimthaihang.com/static/media/ibx2.96c8a99e.jpg"
+  },
+  {
+    "revision": "07346171644237c2459b07d36fa09036",
+    "url": "kimthaihang.com/static/media/kim-portrait.07346171.png"
+  },
+  {
+    "revision": "e2c2da32fa466d65642d5e936295da86",
+    "url": "kimthaihang.com/static/media/medicineshoppe.e2c2da32.jpg"
+  },
+  {
+    "revision": "dd7fb21b0d7bef6414a7e41e0e85fb04",
+    "url": "kimthaihang.com/static/media/mercy3.dd7fb21b.jpg"
+  },
+  {
+    "revision": "a1f9fcf5c055e831dc80353cc7a9f098",
+    "url": "kimthaihang.com/static/media/pcp.a1f9fcf5.jpg"
+  },
+  {
+    "revision": "09c696051ff509ab20cb173d325f8e17",
+    "url": "kimthaihang.com/static/media/penn4.09c69605.jpg"
+  },
+  {
+    "revision": "986334b2a4fb45b0a4c9eeb8730639b0",
+    "url": "kimthaihang.com/static/media/pennsylvania.986334b2.jpg"
+  },
+  {
+    "revision": "5eae2e0b800a3e0f3edbddb16e2471f0",
+    "url": "kimthaihang.com/static/media/ritner3.5eae2e0b.jpg"
+  },
+  {
+    "revision": "128dfd75c9cb63e4d0b276c3c823dc5e",
+    "url": "kimthaihang.com/static/media/silka-regular-webfont.128dfd75.woff"
+  },
+  {
+    "revision": "badc42e2be9598cac13af56e3f726d89",
+    "url": "kimthaihang.com/static/media/silka-regular-webfont.badc42e2.ttf"
+  },
+  {
+    "revision": "c8015761defa0db52e429b7de3641f08",
+    "url": "kimthaihang.com/static/media/silka-regular-webfont.c8015761.eot"
+  },
+  {
+    "revision": "d021db6114442981d146378cc7792d60",
+    "url": "kimthaihang.com/static/media/silka-regular-webfont.d021db61.woff2"
+  },
+  {
+    "revision": "46714fcca8cfaef92491b8af315034b9",
+    "url": "kimthaihang.com/static/media/silka-regularitalic-webfont.46714fcc.eot"
+  },
+  {
+    "revision": "67e0599ac45a1d4a0bdf16b58a58604e",
+    "url": "kimthaihang.com/static/media/silka-regularitalic-webfont.67e0599a.woff2"
+  },
+  {
+    "revision": "9e7b1ff5a2a4416efbfd2e61304e2742",
+    "url": "kimthaihang.com/static/media/silka-regularitalic-webfont.9e7b1ff5.woff"
+  },
+  {
+    "revision": "ba7fa7993a7272c4209a2c89c6e306c4",
+    "url": "kimthaihang.com/static/media/silka-regularitalic-webfont.ba7fa799.ttf"
+  },
+  {
+    "revision": "91453660a7519299e881f6d118bd7b5f",
+    "url": "kimthaihang.com/static/media/specialtypharmacy.91453660.jpg"
+  },
+  {
+    "revision": "76941638aea6aadc7c268476d26488e5",
+    "url": "kimthaihang.com/static/media/usa.76941638.jpg"
+  },
+  {
+    "revision": "81b9a812ea64d27201791988aaee7949",
+    "url": "kimthaihang.com/static/media/walgreens2.81b9a812.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e88b55bf4bdba951ffd12ee54bcc340d.js"
+  "kimthaihang.com/precache-manifest.7de7462e483950fb9f56ea6e01b5b084.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("kimthaihang.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
